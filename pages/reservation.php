84aba731851eba4c3f7a67a617ce0f6055d9b2c9
Olivia <202159967@cegepsherbrooke.qc.ca>
2025-04-01 12:57:33 -0400

    lab3b

--- pages/reservation.php
+++ pages/reservation.php
@@ -29,82 +29,176 @@
         <a href="../index.php">Retour</a>
     </nav>              
     <h1>Formulaire de réservation</h1>
-    <form action="" method="POST">
-        <fieldset class="groupe-champs-textes">
-        <legend>Informations personnelles</legend>
-    
-        <label for="prenom-input">Prénom:</label>
-        <input type="text" id="prenom-input" name="prenom" required />
 
-        <label for="nom-input">Nom:</label>
-        <input type="text" id="nom-input" name="nom" required />
+   <?php
+    $aujourdhui = time();
+    $erreur = false;
+
+    if(
+        //Regarder si toutes les valeurs ont été mises
+        isset($_POST["prenom"]) AND 
+        isset($_POST["nom"]) AND 
+        isset($_POST["adresse"]) AND 
+        isset($_POST["ville"]) AND 
+        isset($_POST["province"]) AND 
+        isset($_POST["codePostal"]) AND 
+        isset($_POST["telephone"]) AND 
+        isset($_POST["courriel"]) AND 
+        isset($_POST["typeChambre"]) AND 
+        isset($_POST["adultes"]) AND 
+        isset($_POST["enfants"]) AND 
+        isset($_POST["dateArrivee"]) AND
+        isset($_POST["nombreNuits"]) AND
+        isset($_POST["submit"]) 
+    ){
+            //Enlever les menances possibles
+            $prenom = strip_tags($_POST["prenom"]);
+            $nom = strip_tags($_POST["nom"]);
+            $adresse = strip_tags($_POST["adresse"]);
+            $ville = strip_tags($_POST["ville"]);
+            $province = strip_tags($_POST["province"]);
+            $codePostal = strip_tags($_POST["codePostal"]);
+            $telephone = strip_tags($_POST["telephone"]);
+            $telephone = preg_replace('/[^0-9]/', '', $telephone);
+            $courriel = strip_tags($_POST["courriel"]);
+            $typeChambre = strip_tags($_POST["typeChambre"]);
+            $submit = strip_tags($_POST["submit"]);
+
+            //Définir le jour, date et mois de la réservation
+            $dateArrivee = strip_tags($_POST["dateArrivee"]);
+            $dateArrivee2 = strtotime($dateArrivee);
+            $jour = date("d", $dateArrivee2);
+            $mois = date("m", $dateArrivee2);
+            $annee = date("Y", $dateArrivee2);
+            
+            if (
+                //Validation des données
+                strlen($prenom) <= 50 AND
+                strlen($nom) <= 50 AND
+                strlen($adresse) <= 50 AND
+                strlen($ville) <= 50 AND
+                array_key_exists($province, $provinces) == 1 AND 
+                strlen($codePostal) == 6 AND 
+                strlen($telephone) == 10 AND is_numeric($telephone) AND
+                ($typeChambre == "1 lit double" OR $typeChambre == "2 lits doubles" OR $typeChambre == "1 lit Queen" OR $typeChambre == "2 lits Queen" OR $typeChambre == "1 lit King") AND 
+                $adultes >= 1 AND $adultes <= 4 AND
+                $enfants <= 4 AND $adultes + $enfants <= 5 AND
+                //valider que la date de réservation soit aujourdhuit ou plus tard et quelle existe
+                $dateArrivee2 && $dateArrivee2 >= $aujourdhui AND 
+                checkdate($mois, $jour, $annee) AND
+                //reserver avant l'annee 2100
+                $annee <= 2100 AND 
+                $nombreNuits >= 0 AND $nombreNuits <= 90
+                ){
+                    //affiche la confirmation de réservation
+                    echo "<h1>Confirmation</h1> <br> 
+                    Votre réservation a été effectuée avec succès. <br><br><br>
+                    <b>Date d'arrivée : </b>", $dateArrivee, "<br><br>
+                    <b>Nombre de nuits : </b>", $nombreNuits, "<br><br>
+                    <b>Type de chambre : </b>", $typeChambre, "<br><br>
+                    <b>Nombre d'adultes : </b>", $adultes, "<br><br>
+                    <b>Nombre d'enfants : </b>", $enfants, "<br><br>";
+
+                    ?><a href="/laboratoire-03-b-Olivia514/pages/reservation.php?">Faire une nouvelle réservation</a><?php
+                    return;
+                }
+            else {
+                //retourner le formulaire avec une mention d'erreur
+                $erreur = true;
+                functionAfficherFormulaire($erreur, $provinces);
+                return;
+            }  
+        }
+        else{
+            //afficher le formulaire
+            functionAfficherFormulaire($erreur, $provinces);
+            return;
+        }
+
+        function functionAfficherFormulaire($erreur, $provinces){
+
+            if($erreur == true){echo 'il a une erreur dans votre formulaure'; }
+
+            ?>
+            <form action="" method="POST">
+            <fieldset class="groupe-champs-textes">
+            <legend>Informations personnelles</legend>
+        
+            <label for="prenom-input">Prénom:</label>
+            <input type="text" id="prenom-input" name="prenom" required />
     
-        <label for="adresse-input">Adresse:</label>
-        <input type="text" id="adresse-input" name="adresse" required />
+            <label for="nom-input">Nom:</label>
+            <input type="text" id="nom-input" name="nom" required />
+        
+            <label for="adresse-input">Adresse:</label>
+            <input type="text" id="adresse-input" name="adresse" required />
+        
+            <label for="ville-input">Ville:</label>
+            <input type="text" id="ville-input" name="ville" required />
+        
+            <label for="province-select">Province / Territoire:</label>
+            <select id="province-select" name="province">
+            <option value="">Sélectionner une province</option>
+            <?php
+                foreach ($provinces as $code => $nomProvince) {
+                echo "<option value=\"$code\">$nomProvince</option>";
+                }
+            ?>
+            </select>
     
-        <label for="ville-input">Ville:</label>
-        <input type="text" id="ville-input" name="ville" required />
+            <label for="code-postal-input">Code postal:</label>
+            <input type="text" id="code-postal-input" name="codePostal" required />
     
-        <label for="province-select">Province / Territoire:</label>
-        <select id="province-select" name="province">
-        <option value="">Sélectionner une province</option>
+            <label for="telephone-input">Numéro de téléphone:</label>
+            <input type="tel" id="telephone-input" name="telephone" placeholder="123-456-7890" required />
+        
+            <label for="courriel-input">Adresse courriel:</label>
+            <input type="text" id="courriel-input" name="courriel" required />
+                                                                                                    
+            </fieldset>
+            <fieldset>
+            <legend>Type de chambre</legend>
+            
+            <input type="radio" id="type-chambre-1-input" name="typeChambre" value="1 lit double" required />
+            <label for="type-chambre-1-input">1 lit double</label>
+    
+            <input type="radio" id="type-chambre-2-input" name="typeChambre" value="2 lits doubles" required />
+            <label for="type-chambre-2-input">2 lits doubles</label>
+    
+            <input type="radio" id="type-chambre-3-input" name="typeChambre" value="1 lit Queen" required />
+            <label for="type-chambre-3-input">1 lit Queen</label>
+    
+            <input type="radio" id="type-chambre-4-input" name="typeChambre" value="2 lits Queen" required />
+            <label for="type-chambre-4-input">2 lits Queen</label>
+            
+            <input type="radio" id="type-chambre-5-input" name="typeChambre" value="1 lit King" required />
+            <label for="type-chambre-5-input">1 lit King</label>            
+            </fieldset>
+            <fieldset class="groupe-champs-textes">
+            <legend>Invités</legend>
+    
+            <label for="nombre-adultes-input">Nombre d'adultes: </label>
+            <input type="number" id="nombre-adultes-input" name="adultes" value="0" />
+    
+            <label for="nombre-enfants-input">Nombre d'enfants: </label>
+            <input type="number" id="nombre-enfants-input" name="enfants" value="0" />
+            </fieldset>
+            <fieldset class="groupe-champs-textes">
+            <legend>Dates</legend>
+    
+            <label for="date-arrivee-input">Date d'arrivée (JJ/MM/AAAA): </label>
+            <input type="text" id="date-arrivee-input" name="dateArrivee" required />
+    
+            <label for="nombre-nuits-input">Nombre de nuits: </label>
+            <input type="number" id="nombre-nuits-input" name="nombreNuits" value="1" />
+            </fieldset>          
+    
+            <fieldset class="groupe-reserver">
+            <input type="submit" name="submit" value="Réserver" />
+            </fieldset>
+        </form>
         <?php
-            foreach ($provinces as $code => $nomProvince) {
-            echo "<option value=\"$code\">$nomProvince</option>";
-            }
-        ?>
-        </select>
-
-        <label for="code-postal-input">Code postal:</label>
-        <input type="text" id="code-postal-input" name="codePostal" required />
-
-        <label for="telephone-input">Numéro de téléphone:</label>
-        <input type="text" id="telephone-input" name="telephone" required />
-    
-        <label for="courriel-input">Adresse courriel:</label>
-        <input type="text" id="courriel-input" name="courriel" required />
-                                                                                                
-        </fieldset>
-        <fieldset>
-        <legend>Type de chambre</legend>
-        
-        <input type="radio" id="type-chambre-1-input" name="typeChambre" value="1 lit double" required />
-        <label for="type-chambre-1-input">1 lit double</label>
-
-        <input type="radio" id="type-chambre-2-input" name="typeChambre" value="2 lits doubles" required />
-        <label for="type-chambre-2-input">2 lits doubles</label>
-
-        <input type="radio" id="type-chambre-3-input" name="typeChambre" value="1 lit Queen" required />
-        <label for="type-chambre-3-input">1 lit Queen</label>
-
-        <input type="radio" id="type-chambre-4-input" name="typeChambre" value="2 lits Queen" required />
-        <label for="type-chambre-4-input">2 lits Queen</label>
-        
-        <input type="radio" id="type-chambre-5-input" name="typeChambre" value="1 lit King" required />
-        <label for="type-chambre-5-input">1 lit King</label>            
-        </fieldset>
-        <fieldset class="groupe-champs-textes">
-        <legend>Invités</legend>
-
-        <label for="nombre-adultes-input">Nombre d'adultes: </label>
-        <input type="number" id="nombre-adultes-input" name="adultes" value="0" />
-
-        <label for="nombre-enfants-input">Nombre d'enfants: </label>
-        <input type="number" id="nombre-enfants-input" name="enfants" value="0" />
-        </fieldset>
-        <fieldset class="groupe-champs-textes">
-        <legend>Dates</legend>
-
-        <label for="date-arrivee-input">Date d'arrivée (JJ/MM/AAAA): </label>
-        <input type="text" id="date-arrivee-input" name="dateArrivee" required />
-
-        <label for="nombre-nuits-input">Nombre de nuits: </label>
-        <input type="number" id="nombre-nuits-input" name="nombreNuits" value="1" />
-        </fieldset>          
-
-        <fieldset class="groupe-reserver">
-        <input type="submit" name="submit" value="Réserver" />
-        </fieldset>
-    </form>
-    </body>
+    }
+?>
+</body>
 </html>
--- pages/tic_tac_toe.php
+++ pages/tic_tac_toe.php
@@ -1,3 +1,5 @@
+<?php session_start();?>
+
 <!DOCTYPE html>
 <html lang="fr">
 <head>
@@ -6,6 +8,9 @@
     <title>Tic Tac Toe</title>
     <link rel="stylesheet" href="../water.css">
     <style>
+        h2{
+            font-size: 40px;
+        }
         .ticTacToe {
             border-collapse: collapse;
             width: auto;
@@ -35,9 +40,114 @@
         <a href="../index.php">Retour</a>
     </nav>
     <h1>Tic Tac Toe</h1>
-    <table class="ticTacToe">
 
+        <?php
+            if (isset($_GET["deconnection"]) AND $_GET["deconnection"] == "oui"){
+                session_unset();
+            }
+
+//définir c'est au tour à qui à jouer
+if(!isset($_SESSION["tour"])){$_SESSION["tour"] = 'X'; $tour = 'X'; echo "c'est au tour au O à jouer";}
+else if($_SESSION["tour"] == 'O'){$_SESSION["tour"] = 'X'; echo "c'est au tour au O à jouer";}
+else{$_SESSION["tour"] = 'O'; echo "c'est au tour au X à jouer";}
+$tour = $_SESSION["tour"];
+
+
+$grille = [
+            ["()", "()", "()"],
+            ["()", "()", "()"],
+            ["()", "()", "()"]
+        ];
+
+//grille à jour
+$nbTour = 0;
+for($i = 0; $i <= 2; $i++){
+    for($j = 0; $j <= 2; $j++){
+        if(!isset($_SESSION['case'.$i. $j])){$grille[$i][$j] = "()";}
+        else{$grille[$i][$j] = $_SESSION['case'.$i. $j];$nbTour++;}
+    }
+}
+
+
+//Ajouter la case qui vient de se faire jouer et vérifier si elle n'a pas déjà été jouée
+function grille($tour, $grille, $nbTour){
+    if(!isset($_GET['case'])){
+        afficherGrille($grille);
+    }
+    else{
+        $page = $_GET['case'];
+        switch($page){
+            case 'case00' : if(!isset($_SESSION['case00'])){$_SESSION['case00'] = $tour; $grille[0][0] = $tour;}; break;
+            case 'case01' : if(!isset($_SESSION['case01'])){$_SESSION['case01'] = $tour; $grille[0][1] = $tour;}; break;
+            case 'case02' : if(!isset($_SESSION['case02'])){$_SESSION['case02'] = $tour; $grille[0][2] = $tour;}; break;
+            case 'case10' : if(!isset($_SESSION['case10'])){$_SESSION['case10'] = $tour; $grille[1][0] = $tour;}; break;
+            case 'case11' : if(!isset($_SESSION['case11'])){$_SESSION['case11'] = $tour; $grille[1][1] = $tour;}; break;
+            case 'case12' : if(!isset($_SESSION['case12'])){$_SESSION['case12'] = $tour; $grille[1][2] = $tour;}; break;
+            case 'case20' : if(!isset($_SESSION['case20'])){$_SESSION['case20'] = $tour; $grille[2][0] = $tour;}; break;
+            case 'case21' : if(!isset($_SESSION['case21'])){$_SESSION['case21'] = $tour; $grille[2][1] = $tour;}; break;
+            case 'case22' : if(!isset($_SESSION['case22'])){$_SESSION['case22'] = $tour; $grille[2][2] = $tour;}; break;
+            default : echo 'ERREUR' ; break;
+        }
+
+
+        //regarder s'il a un gagnant
+        $gagnant = false;
+        for($i = 0; $i <= 2; $i++){
+            //gagnant vertical
+            if(($grille[$i][0] == $grille[$i][1] AND $grille[$i][0] == $grille[$i][2]) AND isset($_SESSION['case'.$i. '0']) AND isset($_SESSION['case'.$i. '1']) AND isset($_SESSION['case'.$i. '2'])){echo '<h2>Les ' . $tour . ' gagnent</h2>'; $gagnant = true; break;}
+            //gagnant horizaontal
+            else if(($grille[0][$i] == $grille[1][$i] AND $grille[0][$i] == $grille[2][$i]) AND isset($_SESSION['case0'.$i]) AND isset($_SESSION['case1'. $i]) AND isset($_SESSION['case2'.$i])){echo '<h2>Les ' . $tour . ' gagnent</h2>'; $gagnant = true; break;}
+        }
+        //gangnant diago 1
+        if($gagnant == true){}
+        else if(($grille[0][0] == $grille[1][1] AND $grille[0][0] == $grille[2][2]) AND isset($_SESSION['case00']) AND isset($_SESSION['case11']) AND isset($_SESSION['case22'])){echo '<h2>Les ' . $tour . ' gagnent</h2>', '?><h2><?php';}
+        //gangnant diago 2
+        else if(($grille[0][2] == $grille[1][1] AND $grille[0][2] == $grille[2][0]) AND isset($_SESSION['case02']) AND isset($_SESSION['case11']) AND isset($_SESSION['case20'])){echo '<h2>Les ' . $tour . ' gagnent</h2>', '?><h2><?php';}
+        //match null
+        else if($nbTour >= 9){echo '<h2>Match null</h2>';}
+
+        //changer le joueur
+        if($_SESSION["tour"] == 'O'){$_SESSION["tour"] == 'X';}
+        else{$_SESSION["tour"] == 'O';}
+        
+        $tour = $_SESSION["tour"];
+        afficherGrille($grille);
+    }
+}
+
+grille($_SESSION["tour"], $grille, $nbTour);
+
+
+function afficherGrille($grille){
+    ?>
+    <table class="ticTacToe">
+        <tr>
+            <td><a href="/laboratoire-03-b-Olivia514/pages/tic_tac_toe.php?case=case00"><?php echo $grille[0][0] ?></a></td>
+            <td><a href="/laboratoire-03-b-Olivia514/pages/tic_tac_toe.php?case=case01"><?php echo $grille[0][1] ?></a></td>
+            <td><a href="/laboratoire-03-b-Olivia514/pages/tic_tac_toe.php?case=case02"><?php echo $grille[0][2] ?></a></td>
+        </tr>
+        <tr>
+            <td><a href="/laboratoire-03-b-Olivia514/pages/tic_tac_toe.php?case=case10"><?php echo $grille[1][0] ?></a></td>
+            <td><a href="/laboratoire-03-b-Olivia514/pages/tic_tac_toe.php?case=case11"><?php echo $grille[1][1] ?></a></td>
+            <td><a href="/laboratoire-03-b-Olivia514/pages/tic_tac_toe.php?case=case12"><?php echo $grille[1][2] ?></a></td>
+        </tr>
+        <tr>
+            <td><a href="/laboratoire-03-b-Olivia514/pages/tic_tac_toe.php?case=case20"><?php echo $grille[2][0] ?></a></td>
+            <td><a href="/laboratoire-03-b-Olivia514/pages/tic_tac_toe.php?case=case21"><?php echo $grille[2][1] ?></a></td>
+            <td><a href="/laboratoire-03-b-Olivia514/pages/tic_tac_toe.php?case=case22"><?php echo $grille[2][2] ?></a></td>
+        </tr>
     </table>
-    <a href="?reinitialiser">Réinitialiser</a>
+    <?php
+}
+
+
+
+
+
+
+
+
+  
+   ?><a href="/laboratoire-03-b-Olivia514/pages/tic_tac_toe.php?deconnection=oui">Réinitialiser</a>
 </body>
 </html>
